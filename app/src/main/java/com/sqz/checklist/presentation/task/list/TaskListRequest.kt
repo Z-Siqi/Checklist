9c26a6cfdb5fcc7a0939fb413e0bb1dcb80e3811
Siqi Zhao <77391690+Z-Siqi@users.noreply.github.com>
2026-04-30 20:46:41 +0800

    Add TaskListRequest interface
    [Implement sealed interface for task list external actions]

---
+++ app/src/main/java/com/sqz/checklist/presentation/task/list/TaskListRequest.kt
@@ -0,0 +1,34 @@
+package com.sqz.checklist.presentation.task.list
+
+/**
+ * Interface for the external action for the task list.
+ *
+ * @see sqz.checklist.task.api.list.model.TaskItemModel.ExternalRequest
+ */
+sealed interface TaskListRequest {
+
+    data object SearchProcessed: TaskListRequest //TODO
+
+    data object RefreshListProcessed: TaskListRequest
+
+    data class Info(
+        val taskId: Long,
+        val pinChangeAllowed: Boolean,
+    ): TaskListRequest
+
+    data class Detail(
+        val taskId: Long
+    ): TaskListRequest
+
+    data class Edit(
+        val taskId: Long
+    ): TaskListRequest
+
+    data class Reminder(
+        val taskId: Long
+    ): TaskListRequest
+
+    data class RemoveReminded(
+        val taskId: Long
+    ): TaskListRequest
+}
